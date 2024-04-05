e3ed0deac15cdd858e22f8dd4cb6b04a84c0b121
Tiago Muniz de Araujo <tiagomuniz130@gmail.com>
2024-04-05 19:34:27 +0000

    views and static path

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { router } from "./routes";
+import path from "node:path";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/", express.static(path.join(__dirname, "..", "public")));
 
 app.use(router);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,15 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { join } from "node:path";
 import { authController } from "../controllers/auth";
 
 const router = Router();
 
+// api endpoints
 router.post("/login", authController);
+
+// views
+router.get("/", (_req: Request, res: Response) => {
+    return res.sendFile(join(__dirname, "..", "view", "index.html"))
+});
 
 export { router };
